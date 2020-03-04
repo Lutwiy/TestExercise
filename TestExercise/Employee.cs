7c7a56668e113488ed92b5391182f08fdb60d527
Lutwiy <lutwiy@gmail.com>
2020-03-04 20:57:39 +0100

    Разбитие по файлам

---
+++ TestExercise/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TestExercise
+{
+    partial class Program
+    {
+        public class Employee
+        {
+            public Guid Id { get; set; }
+            public string FirstName { get; set; }
+            public string SecondName { get; set; }
+
+            public int Pin { get; set; }
+        }
+
+#region Not in task scope
+        #endregion
+    }
+}
