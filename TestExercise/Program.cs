ff5bc650c9e196dff422a88a2e3b44920739f5e0
Lutwiy <lutwiy@gmail.com>
2020-03-04 20:53:18 +0100

    Задание

--- TestExercise/Program.cs
+++ TestExercise/Program.cs
@@ -6,6 +6,11 @@ namespace TestExercise
 {
     class Program
     {
+        /// Задача
+        /// Я, как пользователь, хочу,
+        ///  * чтобы каждый сотрудник, чей Пин делиться без остатка на 7, то рядом с его пином должно быть слово WithBonus,
+        ///  * если делиться на 3 Fired,
+        ///  * а если делиться без остатка и на 3 и на 7, то должно выводиться только слово FiredWithBonus.
         static void Main(string[] args)
         {
             var employees = new EmployeeGenerator().GetEmployees().ToList();
